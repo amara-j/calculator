a178df677f611128b396366dc06d76db22656732
Amara Jaeger <amara.jaeger@gmail.com>
2021-02-08 15:20:23 -0500

    moved onClick functions for each button type into their own functions

--- src/App.js
+++ src/App.js
@@ -9,24 +9,62 @@ function App() {
   const [result, updateResult] = useState(0);
   const operators = ["+", "-", "*", "/", "**"];
 
+  const numberButtonOnClick = (i) => {
+    if (currentOperator === "") {
+      parseInt(operand1) === 0
+        ? updateOperand1(i.toString())
+        : updateOperand1(operand1.toString() + i.toString());
+    } else {
+      parseInt(operand2) === 0
+        ? updateOperand2(i.toString())
+        : updateOperand2(operand2.toString() + i.toString());
+    }
+  };
+
+  const operatorButtonOnClick = (operator) => {
+    if (operand1 === "") {
+      updateOperand1(0);
+    }
+    updateCurrentOperator(operator);
+  };
+
+  const clearButtonOnClick = () => {
+    updateResult(0);
+    updateOperand1("");
+    updateCurrentOperator("");
+  };
+
+  const equalsButtonOnClick = () => {
+    {
+      updateResult(eval(operand1 + currentOperator + operand2));
+      updateOperand1(eval(operand1 + currentOperator + operand2));
+      updateCurrentOperator("");
+      updateOperand2("");
+    }
+  };
+
+  const negativeButtonOnClick = () => {
+    {
+      if (operand1 === "") {
+        updateCurrentOperator("*");
+        updateOperand1("-1");
+      } else {
+        updateCurrentOperator("*");
+        updateOperand2("-1");
+      }
+    }
+  };
+
   const loopNumberComponents = () => {
     let numberComponents = [];
     for (let i = 0; i < 10; i++) {
       numberComponents.push(
-        <Button // generate numbers 1-9 in a loop
+        <Button // generate numbers 0-9 in a loop
           key={i}
           display={i}
           containerClassName={"numberContainer"}
           onClick={() => {
-            if (currentOperator === "") {
-              parseInt(operand1) === 0
-                ? updateOperand1(i.toString())
-                : updateOperand1(operand1.toString() + i.toString());
-            } else {
-              parseInt(operand2) === 0
-                ? updateOperand2(i.toString())
-                : updateOperand2(operand2.toString() + i.toString());
-            }
+            numberButtonOnClick(i);
           }}
         />
       );
@@ -39,15 +77,12 @@ function App() {
       {loopNumberComponents()}
       {operators.map((operator) => {
         return (
-          <Button // generate buttons for +, -, *, / in a loop
+          <Button // generate buttons for +, -, *, /, ** in a loop
             key={operator}
             display={operator}
             containerClassName={"operatorContainer"}
             onClick={() => {
-              if (operand1 === "") {
-                updateOperand1(0);
-              }
-              updateCurrentOperator(operator);
+              operatorButtonOnClick(operator);
             }}
           />
         );
@@ -55,23 +90,23 @@ function App() {
       <Button // generate clear button
         display={"Clear"}
         containerClassName={"clearContainer"}
-        onClick={() => {
-          updateResult(0);
-          updateOperand1("");
-          updateCurrentOperator("");
-        }}
+        onClick={() => clearButtonOnClick()}
       />
       <Button // generate equals button
         display={"Equals"}
         containerClassName={"equalsContainer"}
         onClick={() => {
-          updateResult(eval(operand1 + currentOperator + operand2));
-          updateOperand1(eval(operand1 + currentOperator + operand2));
-          updateCurrentOperator("");
-          updateOperand2("");
+          equalsButtonOnClick();
         }}
       />
 
+      <Button // negative button
+        display={"Negative"}
+        containerClassName={"negativeContainer"}
+        onClick={() => {
+          negativeButtonOnClick();
+        }}
+      />
       <div> {operand1}</div>
       <div>{currentOperator}</div>
       <div> {operand2}</div>
