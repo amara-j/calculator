f84503cbef1774c4b2aa346dc661493e4d9d8d3c
Amara Jaeger <amara.jaeger@gmail.com>
2021-02-08 10:56:54 -0500

    loop through number components in render

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import "./App.css";
 import Number from "./Number.js";
 
 function App() {
-  return (
-    <div className="App">
-      <Number displayNumber={1}></Number>
-      <Number displayNumber={2}></Number>
-      <Number displayNumber={3}></Number>
-      <Number displayNumber={4}></Number>
-      <Number displayNumber={5}></Number>
-      <Number displayNumber={6}></Number>
-      <Number displayNumber={7}></Number>
-    </div>
-  );
+  const loopNumberComponents = () => {
+    let numberComponents = [];
+    for (let i = 0; i < 10; i++) {
+      numberComponents.push(<Number displayNumber={i} />);
+    }
+    return numberComponents;
+  };
+
+  return <div className="App">{loopNumberComponents()}</div>;
 }
 
 export default App;
