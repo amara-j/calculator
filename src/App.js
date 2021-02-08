6028c389f53ca53ab2adecf20c32dc7456a61830
Amara Jaeger <amara.jaeger@gmail.com>
2021-02-08 16:59:40 -0500

    backspace keydown deletes last character of entered expression

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [operand1, updateOperand1] = useState("");
   const [operator, updateOperator] = useState("");
   const [operand2, updateOperand2] = useState("");
+  let expression = operand1 + operator + operand2;
   const [result, updateResult] = useState(0);
   const operator_array = ["+", "-", "*", "/", "**"];
 
@@ -28,8 +29,8 @@ function App() {
     updateOperator(element);
     //if operator already full,
     if (operator != "") {
-      updateResult(eval(operand1 + operator + operand2));
-      updateOperand1(eval(operand1 + operator + operand2));
+      updateResult(operand1 + operator + operand2);
+      updateOperand1(operand1 + operator + operand2);
       updateOperator(element);
       updateOperand2("");
     }
@@ -44,8 +45,8 @@ function App() {
 
   const equalsButtonOnClick = () => {
     {
-      updateResult(eval(operand1 + operator + operand2));
-      updateOperand1(eval(operand1 + operator + operand2));
+      updateResult(operand1 + operator + operand2);
+      updateOperand1(operand1 + operator + operand2);
       updateOperator("");
       updateOperand2("");
     }
@@ -84,6 +85,38 @@ function App() {
     return numberComponents;
   };
 
+  const handleDelete = () => {
+    if (operand2.length > 0) {
+      updateOperand2(operand2.substring(0, operand2.length - 1));
+    } else if (operator != "") {
+      updateOperator("");
+    } else if (operand1.length > 0) {
+      updateOperand1(operand1.substring(0, operand1.length - 1));
+    }
+  };
+
+  const handleKeyDown = (e) => {
+    console.log(e);
+    // numbers
+    if (e.code.slice(0, 5) === "Digit" && e.shiftKey === false) {
+      numberButtonOnClick(e.code.slice(-1));
+    }
+    if (operator_array.includes(e.key)) {
+      operatorButtonOnClick(e.key);
+    }
+    if (e.code === "Enter") {
+      equalsButtonOnClick();
+    }
+    if (e.code === "Backspace") {
+      handleDelete();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
+  }, [handleKeyDown]);
+
   return (
     <div className="App">
       {loopNumberComponents()}
@@ -118,8 +151,9 @@ function App() {
           negativeButtonOnClick();
         }}
       />
-      <div className="resultContainer">Result: {result}</div>{" "}
+      <div className="resultContainer">Result: {eval(result)}</div>{" "}
       <div> {operand1 + operator + operand2}</div>
+      <div>{expression}</div>
     </div>
   );
 }
