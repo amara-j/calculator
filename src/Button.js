7ae4d6d0c48d76b51617cd34ee88c4b3f8c0697d
Amara Jaeger <amara.jaeger@gmail.com>
2021-02-11 13:01:29 -0500

    adding generic button css class for common button styles

--- src/Button.js
+++ src/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 const Button = (props) => {
   return (
     <div
-      className={props.containerClassName}
+      className={`${props.containerClassName} button`}
       id={props.containerID}
       onClick={props.onClick}
     >
